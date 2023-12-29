dfbdfab19c05f518feb50e1cc22d8bafd6dbaa1b
VolumiDev <diego.martga@gmail.com>
2023-12-29 01:12:19 +0100

    Primera version de la WEB de MARCAS

---
+++ js/personajes.js
@@ -0,0 +1,30 @@
+//FUNCIONES PARA ALDURON
+function mostrarVentana() {
+    // Mostrar la ventana emergente
+    document.getElementById('ventanaEmergente').style.display = 'block';
+}
+
+function cerrarVentana() {
+    // Cerrar la ventana emergente
+    document.getElementById('ventanaEmergente').style.display = 'none';
+}
+//FUNCIONES PARA SHAENA
+function mostrarVentana2() {
+    // Mostrar la ventana emergente
+    document.getElementById('ventanaEmergente2').style.display = 'block';
+}
+
+function cerrarVentana2() {
+    // Cerrar la ventana emergente
+    document.getElementById('ventanaEmergente2').style.display = 'none';
+}
+//FUNCIONES PARA NAIVARA
+function mostrarVentana5() {
+                 // Mostrar la ventana emergente
+    document.getElementById('ventanaEmergente5').style.display = 'block';
+}
+
+function cerrarVentana5() {
+                 // Cerrar la ventana emergente
+    document.getElementById('ventanaEmergente5').style.display = 'none';
+}
