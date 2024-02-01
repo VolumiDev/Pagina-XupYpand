f21f815082e1bfe89f90d29f3be48f57326a4a1d
VolumiDev <diego.martga@gmail.com>
2024-02-01 13:15:24 +0100

    ventanas emergentes y personajes terminados

--- js/personajes.js
+++ js/personajes.js
@@ -18,6 +18,27 @@ function cerrarVentana2() {
     // Cerrar la ventana emergente
     document.getElementById('ventanaEmergente2').style.display = 'none';
 }
+//FUNCIONES PARA GABRIN
+function mostrarVentana3() {
+    // Mostrar la ventana emergente
+document.getElementById('ventanaEmergente3').style.display = 'block';
+}
+
+function cerrarVentana3() {
+    // Cerrar la ventana emergente
+document.getElementById('ventanaEmergente3').style.display = 'none';
+}
+
+//FUNCIONES PARA AMBER  
+function mostrarVentana4() {
+    // Mostrar la ventana emergente
+document.getElementById('ventanaEmergente4').style.display = 'block';
+}
+
+function cerrarVentana4() {
+    // Cerrar la ventana emergente
+document.getElementById('ventanaEmergente4').style.display = 'none';
+}
 //FUNCIONES PARA NAIVARA
 function mostrarVentana5() {
                  // Mostrar la ventana emergente
@@ -27,4 +48,14 @@ function mostrarVentana5() {
 function cerrarVentana5() {
                  // Cerrar la ventana emergente
     document.getElementById('ventanaEmergente5').style.display = 'none';
+}
+//FUNCIONES PARA VANIR
+function mostrarVentana6() {
+    // Mostrar la ventana emergente
+document.getElementById('ventanaEmergente6').style.display = 'block';
+}
+
+function cerrarVentana6() {
+    // Cerrar la ventana emergente
+document.getElementById('ventanaEmergente6').style.display = 'none';
 }
